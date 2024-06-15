5323c4804642f842bb4a78c231b61b2e0d75677e
salmanalfarisir <154666430+salmanalfarisir@users.noreply.github.com>
2024-06-16 01:05:36 +0700

    first commit my all project in dicoding

---
+++ app/src/main/java/com/cafstone/application/view/detail/DetailSectionPager.kt
@@ -0,0 +1,22 @@
+package com.cafstone.application.view.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class DetailSectionPager(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        var fragment: Fragment? = null
+        when (position) {
+            0 -> fragment = RingkasanFragment()
+            1 -> fragment = UlasanFragment()
+            2 -> fragment = TentangFragment()
+        }
+        return fragment as Fragment
+    }
+
+}
